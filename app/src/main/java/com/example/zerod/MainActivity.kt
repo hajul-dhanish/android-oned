5397326ec8b37db4e1b15d0c68ab487c96efcde1
hajul-dhanish <dhanish.mohdn@gmail.com>
2022-03-23 17:31:32 +0530

    with kotlin

--- app/src/main/java/com/example/zerod/MainActivity.kt
+++ app/src/main/java/com/example/zerod/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.zerod
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val userName: String = mainUserName.text.toString()
+//        val emailAdd: String = emailid.text.toString()
+//        val passWord: String = passid.text.toString()
+
+//        val radioCkAns = radioGrp.checkedRadioButtonId
+//        val radioButton = findViewById<RadioButton>(radioCkAns)
+
+
+        createAccid.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                "Will Update in Upcoming Version...",
+                Toast.LENGTH_SHORT
+            ).show() }
+
+            skipToMainPageid.setOnClickListener {
+                Toast.makeText(this, "On the Way fam :) ", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
